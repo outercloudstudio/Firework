2eb2adcef3b63f3639e2de04fd055bda3a048f1c
outercloudstudio <outercloudstudio@gmail.com>
2022-02-04 16:06:13 -0600

    upd: First Version of CLI

--- Firework.js
+++ Firework.js
@@ -1,12 +1,15 @@
-const util = require('util')
-const fs = require('fs')
+import * as util from 'util'
+import * as fs from 'fs'
+import * as paths from 'path'
 
-const Tokenizer = require('./Tokenizer')
-const ExecutionTree = require('./ExecutionTree')
-const Compiler = require('./Compiler')
-const Backend = require('./Backend')
+//import * as Tokenizer from './Tokenizer.js'
+//import * as ExecutionTree from './ExecutionTree.js'
+//import * as Compiler from './Compiler.js'
 
-function Compile(){
+import chalk from 'chalk'
+import { createSpinner } from 'nanospinner'
+
+/*function Compile(){
   const tokens = Tokenizer.tokenize(fs.readFileSync('./Delay.frw').toString())
 
   const ETree = ExecutionTree.generateETree(tokens)
@@ -24,6 +27,53 @@ function Compile(){
 
     return
   }
+}*/
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-module.exports = { Compile };
+export async function Start(path, com){
+  console.log(chalk.hex('#5ac54f').bold('Starting compilation server v1.0.0!'))
+
+  const spinner = createSpinner('Waiting for bridge to compile...').start()
+    
+  while(!fs.existsSync(com + '/development_behavior_packs/' + paths.basename(path) + ' BP') || !fs.existsSync(com + '/development_behavior_packs/' + paths.basename(path) + ' BP/manifest.json')){
+    await(sleep(1000))
+  }
+
+  spinner.success()
+
+  while(true){
+    await sleep(100)
+
+    while(!fs.existsSync(com + '/development_behavior_packs/' + paths.basename(path) + ' BP') || !fs.existsSync(com + '/development_behavior_packs/' + paths.basename(path) + ' BP/manifest.json')){
+      await(sleep(100))
+    }
+
+    try{
+      let mainfest = null
+
+      while(!mainfest){
+        try{
+          mainfest = JSON.parse(fs.readFileSync(com + '/development_behavior_packs/' + paths.basename(path) + ' BP/manifest.json'))
+        }catch{}
+
+        await(sleep(100))
+      }
+      
+      if(!mainfest.firework){
+        console.log(chalk.hex('#ffc825').bold('Warning:'), 'Detected recompile!')
+      }
+
+      mainfest.firework = {
+        
+      }
+
+      fs.writeFileSync(com + '/development_behavior_packs/' + paths.basename(path) + ' BP/manifest.json', JSON.stringify(mainfest, null, 2))
+    }catch (err){
+      console.log(chalk.hex('#ffc825').bold('Warning:'), 'Ignored error!')
+      console.log(err.toString())
+    }
+  }
+}
